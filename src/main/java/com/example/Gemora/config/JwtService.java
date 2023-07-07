7b3d82e9fa0dc5d5ec807fe5d009d196fe38b997
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-07 18:18:07 +0200

    Feature: Created JwtService Class

---
+++ src/main/java/com/example/Gemora/config/JwtService.java
@@ -0,0 +1,17 @@
+package com.example.Gemora.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+
+}
