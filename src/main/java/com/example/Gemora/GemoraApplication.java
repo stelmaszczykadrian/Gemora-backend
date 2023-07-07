4f4e1e22f0d46d6079776f1d285d011ac2f719b3
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-07 17:31:43 +0200

    Initial repo

---
+++ src/main/java/com/example/Gemora/GemoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gemora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gemora/GemoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gemora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
