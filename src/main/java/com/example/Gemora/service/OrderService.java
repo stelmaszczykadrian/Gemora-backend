ac9f05b52d105dd27891fc74e3d532a227f65a79
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-12 15:35:15 +0200

    Feature: Implement OrderService with CRUD operations

---
+++ src/main/java/com/example/Gemora/service/OrderService.java
@@ -0,0 +1,48 @@
+package com.example.Gemora.service;
+
+import com.example.Gemora.model.Order;
+import com.example.Gemora.model.Product;
+import com.example.Gemora.repository.OrderRepository;
+import com.example.Gemora.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(List<Product> products, User user) {
+        LocalDateTime orderDateTime = LocalDateTime.now();
+        double totalAmount = calculateTotalAmount(products);
+        Order order = Order.builder()
+                .products(products)
+                .user(user)
+                .orderDateTime(orderDateTime)
+                .totalAmount(totalAmount)
+                .build();
+        return orderRepository.save(order);
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(int orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalStateException("Order not found with id: " + orderId));
+    }
+
+    private double calculateTotalAmount(List<Product> products) {
+        double totalAmount = 0;
+        for (Product product : products) {
+            totalAmount += product.getPrice();
+        }
+        return totalAmount;
+    }
+}
