8ce2fa2efb65ae546a7fc30a43254cb3d0babeb1
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-12 15:27:36 +0200

    Feature: Implement Order class with associated methods

---
+++ src/main/java/com/example/Gemora/model/Order.java
@@ -0,0 +1,35 @@
+package com.example.Gemora.model;
+
+import com.example.Gemora.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    private List<Product> products;
+
+    @ManyToOne
+    private User user;
+
+    @Column(name = "order_date_time")
+    private LocalDateTime orderDateTime;
+
+    @Column(name = "total_amount")
+    private double totalAmount;
+}
