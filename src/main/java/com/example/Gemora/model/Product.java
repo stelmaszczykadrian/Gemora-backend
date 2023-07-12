44ec91391797c8569ba32e846a6881be6c641b36
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-12 14:11:55 +0200

    Feature: Implement Product class to represent jewelry products

---
+++ src/main/java/com/example/Gemora/model/Product.java
@@ -0,0 +1,25 @@
+package com.example.Gemora.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private double price;
+    private String manufacturer;
+    private String description;
+    private String category;
+    private boolean availability;
+}
