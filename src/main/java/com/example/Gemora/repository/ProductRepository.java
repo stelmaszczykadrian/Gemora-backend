2e9c25eac339f67bc46012aa53846d1e55af39d7
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-12 14:29:00 +0200

    Feature: Implement ProductService with CRUD operations

--- src/main/java/com/example/Gemora/repository/ProductRepository.java
+++ src/main/java/com/example/Gemora/repository/ProductRepository.java
@@ -2,8 +2,9 @@ package com.example.Gemora.repository;
 
 import com.example.Gemora.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 }
 
---
+++ src/main/java/com/example/Gemora/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.example.Gemora.service;
+
+import com.example.Gemora.model.Product;
+import com.example.Gemora.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> getProductById(int id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Product product) {
+        productRepository.delete(product);
+    }
+}
