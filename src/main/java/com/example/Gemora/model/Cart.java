51447bf5015856f2ca4df90c5abead9d999a059f
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-20 17:29:38 +0200

    Refactor: Add Cart entity and constructor annotations

--- src/main/java/com/example/Gemora/model/Cart.java
+++ src/main/java/com/example/Gemora/model/Cart.java
@@ -1,14 +1,26 @@
 package com.example.Gemora.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Table(name = "carts")
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToMany(mappedBy = "cart")
     private List<Product> products;
 
     public void addProduct(Product product) {
