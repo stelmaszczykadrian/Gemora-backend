f31489f483ee85d97702fcff5dcecbec06345c81
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-10 15:46:32 +0200

    Feature: Implement logic to create product from form data

--- src/main/java/com/example/Gemora/product/ProductController.java
+++ src/main/java/com/example/Gemora/product/ProductController.java
@@ -5,40 +5,64 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.Gemora.product.Product;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable int id) {
-        // TODO: Implement logic to retrieve product by id from ProductService
-        return null;
+    public Optional<Product> getProductById(@PathVariable int id) {
+        return productService.getProductById(id);
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
+    public List<Product> getAllProducts() {
         // TODO: Implement logic to retrieve all products from ProductService
         return null;
     }
 
+
     @PostMapping
-    public ResponseEntity<Void> createProduct(@RequestBody Product product) {
-        // TODO: Implement logic to create a new product using ProductService
-        return null;
+    public void createProduct(
+            @RequestParam("name") String name,
+            @RequestParam("price") double price,
+            @RequestParam("manufacturer") String manufacturer,
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("description") String description,
+            @RequestParam("category") String category) throws IOException {
+
+        Product product = Product.builder()
+                .name(name)
+                .price(price)
+                .category(category)
+                .description(description)
+                .image(image.getBytes())
+                .manufacturer(manufacturer)
+                .build();
+
+        productService.saveProduct(product);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateProduct(@PathVariable int id, @RequestBody Product product) {
+    public void updateProduct(@PathVariable int id, @RequestBody Product product) {
         // TODO: Implement logic to update product by id using ProductService
-        return null;
+
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable int id) {
+    public void deleteProduct(@PathVariable int id) {
         // TODO: Implement logic to delete product by id using ProductService
-        return null;
+
     }
 }
