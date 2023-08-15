16966f84569f1be991eeb92c0d9459533d81db11
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-15 16:38:34 +0200

    Feature: Added UserRepository functions for retrieving user's cart items

--- src/main/java/com/example/Gemora/cart/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Gemora.cart;
-
-import com.example.Gemora.cart.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-}
