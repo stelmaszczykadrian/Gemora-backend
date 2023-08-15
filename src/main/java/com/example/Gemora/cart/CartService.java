febb6b08fd4723779e19621976f995ab86d40785
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-15 16:40:02 +0200

    Feature: Added logic for adding items to the user's cart

--- src/main/java/com/example/Gemora/cart/CartService.java
+++ src/main/java/com/example/Gemora/cart/CartService.java
@@ -1,40 +1,59 @@
 package com.example.Gemora.cart;
 
-import com.example.Gemora.cart.Cart;
-import com.example.Gemora.product.Product;
-import com.example.Gemora.cart.CartRepository;
+import com.example.Gemora.user.User;
+import com.example.Gemora.user.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService {
-    private final CartRepository cartRepository;
+    private CartItemRepository cartItemRepository;
+    private UserRepository userRepository;
 
-    public CartService(CartRepository cartRepository) {
-        this.cartRepository = cartRepository;
+
+    public CartService(CartItemRepository cartItemRepository, UserRepository userRepository) {
+        this.cartItemRepository = cartItemRepository;
+        this.userRepository = userRepository;
     }
 
-    public void addToCart(Cart cart, Product product) {
-        cart.addProduct(product);
-        cartRepository.save(cart);
+    public List<CartItem> getCartItemsForUser(String userEmail) {
+        Optional<User> user = userRepository.findByEmail(userEmail);
+        if (user.isPresent()) {
+            return cartItemRepository.findByUserId(user.get().getId());
+        } else {
+            return Collections.emptyList();
+        }
     }
 
-    public void removeFromCart(Cart cart, Product product) {
-        cart.removeProduct(product);
-        cartRepository.save(cart);
+    public void addToCart(String userEmail, Integer productId, int quantity) {
+
+        Optional<User> user = userRepository.findByEmail(userEmail);
+
+        if (user.isPresent()) {
+            Integer userId = user.get().getId();
+
+            List<CartItem> existingItems = cartItemRepository.findByUserIdAndProductId(userId, productId);
+
+            if (!existingItems.isEmpty()) {
+
+                CartItem existingItem = existingItems.get(0);
+                existingItem.setQuantity(existingItem.getQuantity() + quantity);
+                cartItemRepository.save(existingItem);
+            } else {
+                CartItem newItem = new CartItem();
+                newItem.setUserId(userId);
+                newItem.setProductId(productId);
+                newItem.setQuantity(quantity);
+                cartItemRepository.save(newItem);
+            }
+        } else {
+            // TODO // Handle the case when the user does not exist
+            // For example, you can throw an exception or take another action
+        }
     }
 
-    public void clearCart(Cart cart) {
-        cart.clearCart();
-        cartRepository.save(cart);
-    }
 
-    public List<Product> getCartProducts(Cart cart) {
-        return cart.getProducts();
-    }
-
-    public double getCartTotalPrice(Cart cart) {
-        return cart.getTotalPrice();
-    }
 }
