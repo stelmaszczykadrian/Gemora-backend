642091b970fccb411001361a08a3febc7df79742
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-09-26 20:27:42 +0200

    Feature: Update ProductDto class to include fields: id, name, price, manufacturer, description, category, and image

--- src/main/java/com/example/Gemora/product/ProductDto.java
+++ src/main/java/com/example/Gemora/product/ProductDto.java
@@ -1,4 +1,16 @@
 package com.example.Gemora.product;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ProductDto {
+    private Integer id;
+    private String name;
+    private double price;
+    private String manufacturer;
+    private String description;
+    private String category;
+    private String image;
 }
