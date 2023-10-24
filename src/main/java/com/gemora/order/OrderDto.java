aa71565c9e5e3b50949469cc7ebec741794a96e8
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-24 19:38:36 +0200

    Feature: Expand OrderDto class with additional fields and annotations

--- src/main/java/com/gemora/order/OrderDto.java
+++ src/main/java/com/gemora/order/OrderDto.java
@@ -1,4 +1,19 @@
 package com.gemora.order;
 
+import com.gemora.product.ProductDto;
+import com.gemora.user.UserDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
 public class OrderDto {
+    private Integer id;
+    private List<ProductDto> products;
+    private UserDto user;
+    private LocalDateTime orderDateTime;
+    private double totalAmount;
 }
