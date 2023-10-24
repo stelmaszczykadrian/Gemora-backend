027fd546a57cdde4174c6a457cff618a77402e22
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-24 19:45:52 +0200

    Refactor: Enhance ProductService with improved structure and data mapping

--- src/main/java/com/gemora/product/ProductService.java
+++ src/main/java/com/gemora/product/ProductService.java
@@ -3,10 +3,8 @@ package com.gemora.product;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Base64;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,72 +15,91 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public Optional<Product> getProductById(int id) {
-        return productRepository.findById(id);
+    public Optional<ProductDto> getProductById(int id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+
+        return productOptional.map(this::mapProductToDto);
     }
 
-    public List<Product> getAllProducts(String sortType) {
+    public List<ProductDto> getAllProducts(String sortType) {
         List<Product> products = productRepository.findAll();
         sortProducts(products, sortType);
-        return products;
-
-    }
-
-    public void saveProduct(Product product) {
-        productRepository.save(product);
-    }
-
-    public void updateProduct(Product product) {
-        productRepository.save(product);
-    }
-
-    public void deleteProduct(Product product) {
-        productRepository.delete(product);
-    }
-
-    @Transactional
-    public List<Product> getProductsByCategory(String category) {
-        return productRepository.findByCategory(category);
-    }
-
-    @Transactional
-    public List<ProductDto> getSortedProducts(String category, String sortType) {
-        List<Product> products = productRepository.findByCategory(category);
-        sortProducts(products, sortType);
-        return products.stream().map(product ->
-                        ProductDto.builder()
-                                .name(product
-                                        .getName())
-                                .manufacturer(product.getManufacturer())
-                                .price(product.getPrice())
-                                .description(product.getDescription())
-                                .category(product.getCategory())
-                                .image(Base64.getEncoder().encodeToString(product.getImage()))
-                                .build())
+        return products.stream()
+                .map(this::mapProductToDto)
                 .collect(Collectors.toList());
     }
 
     @Transactional
-    public List<Product> getFeaturedProducts() {
-        return productRepository.findByCategory("FEATURED");
+    public List<ProductDto> getProductsByCategory(String category) {
+        ProductCategory categoryEnum = ProductCategory.from(category);
+
+        List<Product> products = productRepository.findByCategory(categoryEnum.name());
+
+        return products.stream()
+                .map(this::mapProductToDto)
+                .collect(Collectors.toList());
     }
 
-    private void sortProducts(List<Product> products, String sortType) {
+    @Transactional
+    public List<ProductDto> getSortedProducts(String category, String sortType) {
+        ProductCategory categoryEnum = ProductCategory.from(category);
+
+        List<Product> products = productRepository.findByCategory(categoryEnum.name());
+
+        sortProducts(products, sortType);
+        return products.stream()
+                .map(this::mapProductToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<ProductDto> getFeaturedProducts() {
+        ProductCategory category = ProductCategory.FEATURED;
+
+        List<Product> products = productRepository.findByCategory(category.name());
+
+        return products.stream()
+                .map(this::mapProductToDto)
+                .collect(Collectors.toList());
+    }
+
+    public void sortProducts(List<Product> products, String sortType) {
         SortType enumSortType = SortType.from(sortType);
 
         switch (enumSortType) {
-            case ASCENDING:
-                products.sort(Comparator.comparing(Product::getPrice));
-                break;
-            case DESCENDING:
-                products.sort(Comparator.comparing(Product::getPrice).reversed());
-                break;
-            case NEWEST:
-                products.sort(Comparator.comparing(Product::getPostingDate).reversed());
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported sort type: " + sortType);
+            case ASCENDING -> products.sort(Comparator.comparing(Product::getPrice));
+            case DESCENDING -> products.sort(Comparator.comparing(Product::getPrice).reversed());
+            case NEWEST -> products.sort(Comparator.comparing(Product::getPostingDate).reversed());
         }
     }
 
+    private ProductDto mapProductToDto(Product product) {
+        return ProductDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .manufacturer(product.getManufacturer())
+                .description(product.getDescription())
+                .category(product.getCategory())
+                .image(Base64.getEncoder().encodeToString(product.getImage()))
+                .build();
+    }
+
+    public void createProduct(ProductRequest productRequest) {
+        if (productRequest == null) {
+            throw new IllegalArgumentException("Product request cannot be null");
+        }
+
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .price(productRequest.getPrice())
+                .category(productRequest.getCategory())
+                .description(productRequest.getDescription())
+                .image(Base64.getDecoder().decode(productRequest.getImage()))
+                .manufacturer(productRequest.getManufacturer())
+                .postingDate(LocalDateTime.now())
+                .build();
+
+        productRepository.save(product);
+    }
 }
