ee47240543f8d6bd0db6a18b7b54fe5a78eccd9a
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-11-23 22:33:03 +0100

    GEM-73: Add OrderDto class for order data representation and transfer

--- src/main/java/com/gemora/order/OrderDto.java
+++ src/main/java/com/gemora/order/OrderDto.java
@@ -1,19 +1,26 @@
 package com.gemora.order;
 
-import com.gemora.product.ProductDto;
 import com.gemora.user.UserDto;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Builder
 public class OrderDto {
     private Integer id;
-    private List<ProductDto> products;
+    private List<OrderSimplifiedProduct> products;
     private UserDto user;
     private LocalDateTime orderDateTime;
     private double totalAmount;
+    private ShippingDetails shippingDetails;
+
+    public OrderDto(Order order) {
+        this.id = order.getId();
+        this.products = order.getProducts();
+        this.user = new UserDto(order.getUser());
+        this.orderDateTime = order.getOrderDateTime();
+        this.totalAmount = order.getTotalAmount();
+        this.shippingDetails = order.getShippingDetails();
+    }
 }
