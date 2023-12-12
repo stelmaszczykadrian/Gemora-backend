6929867a6da36b96e6d2f12f3a2727a3917cfbf3
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-12 19:44:28 +0100

    GEM-102: Add User end-to-end tests

---
+++ src/test/java/com/Gemora/e2e/user/UserE2ETest.java
@@ -0,0 +1,61 @@
+package com.Gemora.e2e.user;
+
+import com.gemora.security.token.TokenRepository;
+import com.gemora.user.Role;
+import com.gemora.user.User;
+import com.gemora.user.UserDto;
+import com.gemora.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserE2ETest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @BeforeEach
+    void setUp() {
+        tokenRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void getUserByEmail_ReturnsUserDto_UserExist() {
+        //given
+        String userEmail = "johndoe@gmail.com";
+
+        String baseUrl = "http://localhost:" + port + "/api/users/profile/" + userEmail;
+
+        User user = new User(1,"John", "Doe", "johndoe@gmail.com", "abcdef", Role.USER);
+        userRepository.save(user);
+
+        //when
+        ResponseEntity<UserDto> responseEntity = restTemplate.getForEntity(baseUrl, UserDto.class);
+
+        //then
+        UserDto userDto = responseEntity.getBody();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(userDto);
+        assertEquals("John", userDto.getFirstname());
+        assertEquals("johndoe@gmail.com", userDto.getEmail());
+    }
+}
