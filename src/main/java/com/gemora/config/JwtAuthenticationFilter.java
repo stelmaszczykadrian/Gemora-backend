912e69249336cc22b04afbed7ae1bdd6a3562aa7
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-12 20:41:59 +0100

    GEM-106: Integration H2 Database for end-to-end testing and remove blank space on JwtAuthenticationFilter

--- src/main/java/com/gemora/config/JwtAuthenticationFilter.java
+++ src/main/java/com/gemora/config/JwtAuthenticationFilter.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
     private final TokenRepository tokenRepository;
