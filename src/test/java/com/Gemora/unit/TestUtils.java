e9ac6fcfcf35a1d55ff0c83ba1909cea8b4f97f9
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-14 19:35:50 +0100

    GEM-128: Refactor TestUtils to use autowired ObjectMapper

--- src/test/java/com/Gemora/unit/TestUtils.java
+++ src/test/java/com/Gemora/unit/TestUtils.java
@@ -1,12 +1,22 @@
 package com.Gemora.unit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@Component
 public class TestUtils {
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public TestUtils(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     public static BindingResult getBindingResult(boolean t) {
         BindingResult bindingResult = mock(BindingResult.class);
         when(bindingResult.hasErrors()).thenReturn(t);
@@ -16,6 +26,14 @@ public class TestUtils {
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String asJsonString2(final Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
