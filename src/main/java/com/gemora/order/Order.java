a3fa740353cb939f34fadc32d691955922e1d7a7
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-14 19:21:43 +0100

    GEM-122: Update validation constraints in Order

--- src/main/java/com/gemora/order/Order.java
+++ src/main/java/com/gemora/order/Order.java
@@ -2,6 +2,9 @@ package com.gemora.order;
 
 import com.gemora.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,19 +24,23 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Valid
     @ElementCollection
     @CollectionTable(name = "order_items", joinColumns = @JoinColumn(name = "order_id"))
     private List<OrderSimplifiedProduct> products;
 
+    @NotNull(message = "User cannot be null")
     @ManyToOne
     private User user;
 
     @Column(name = "order_date_time")
     private LocalDateTime orderDateTime = LocalDateTime.now();
 
+    @Min(value = 0, message = "Total amount cannot be negative")
     @Column(name = "total_amount")
     private double totalAmount;
 
+    @Valid
     @Embedded
     private ShippingDetails shippingDetails;
 }
