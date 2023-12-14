bfe7303197aa461a97b017791eef730a76434dd3
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-14 19:24:05 +0100

    GEM-124: Update OrderController with validation, adjust response status and add getOrdersByUserId method

--- src/main/java/com/gemora/order/OrderController.java
+++ src/main/java/com/gemora/order/OrderController.java
@@ -1,44 +1,73 @@
 package com.gemora.order;
 
-import com.gemora.payu.OrderCreateRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.gemora.validation.ValidationHelper.handleBindingResultErrors;
 
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
-
     private final OrderService orderService;
-
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
     @PostMapping
-    public ResponseEntity<String> createOrder(
-            @RequestBody OrderCreateRequest request) {
-
+    public ResponseEntity<String> createOrder(@RequestBody OrderCreateRequest request) {
         OrderCreateResponse orderCreate = orderService.createOrder(request);
 
-
-        return orderCreate.isSuccess()?new ResponseEntity<>(orderCreate.getData(),HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR) ;
+        if (orderCreate.isSuccess()) {
+            return new ResponseEntity<>(orderCreate.getData(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/save-order")
-    public void saveOrder(@RequestBody Order order) {
-        orderService.saveOrder(order);
+    public ResponseEntity<String> saveOrder(@Valid @RequestBody Order order, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return handleBindingResultErrors(bindingResult);
+        }
+
+        try {
+            orderService.saveOrder(order);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Order added successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @GetMapping
-    public List<OrderDto> getAllOrders() {
-        return orderService.getAllOrders();
+    public ResponseEntity<List<OrderDto>> getAllOrders() {
+        List<OrderDto> allOrders = orderService.getAllOrders();
+
+        if (allOrders.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(allOrders);
+        }
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<List<OrderDto>> getOrdersByUserId(
+            @PathVariable Integer id) {
+        List<OrderDto> userOrders  = orderService.getOrdersByUserId(id);
+
+        if (!userOrders.isEmpty()) {
+            return ResponseEntity.ok(userOrders);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
 
 
--- src/main/java/com/gemora/order/OrderRepository.java
+++ src/main/java/com/gemora/order/OrderRepository.java
@@ -3,6 +3,9 @@ package com.gemora.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findOrdersByUserId(Integer userId);
 }
